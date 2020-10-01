883dfbfea5fcc0bfce880b35eb235f3c042f2fca
khushbook19pa <63450932+khushbook19pa@users.noreply.github.com>
2020-10-01 21:47:10 +0530

    Add files via upload

---
+++ pre and post  increment .cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the value of a";
+	cin>>a;
+	cout<<"Enter the value of b";
+	cin>>b;
+	cout<<++a;
+	cout<<"\n";
+	cout<<b++;
+	return 0;
+}
